1de4ca338d74c30d6d95a180dddc43dd82753e58
Vignesh <Vignesh@DESKTOP-I1RC45B>
2023-03-28 03:36:04 -0700

    Selenium Extent Reports

---
+++ src/mar24/Demo1.java
@@ -0,0 +1,21 @@
+package mar24;
+
+public class Demo1 {
+public void add()
+{
+	int a=4567,b=5,c;
+	c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=32,b=4,c;
+	c=a+b;
+	System.out.println(c);
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
